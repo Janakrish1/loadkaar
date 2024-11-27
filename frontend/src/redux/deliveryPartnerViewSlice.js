728bc892331909a68a19689ab7da7d03fcea1295
Jana <janakrish3011@gmail.com>
2024-11-27 15:50:03 -0500

    Added employee cards in employer home page

--- frontend/src/redux/deliveryPartnerViewSlice.js
+++ frontend/src/redux/deliveryPartnerViewSlice.js
@@ -4,17 +4,24 @@ const deliveryPartnerViewSlice = createSlice({
     name: 'deliveryPartnerView',
     initialState: {
         currentView: "default",
+        activeMenu: "Current Orders",
     },
     reducers: {
         setDeliveryPartnerView: (state, action) => {
-            state.currentView = action.payload.currentView;
+            console.log("HERERERE", action.payload);
+            state.currentView = typeof action.payload === "string" ? action.payload : action.payload.currentView;
+            state.activeMenu = typeof action.payload === "string" ? action.payload : action.payload.activeMenu;
         },
 
         clearDeliveryPartnerView: (state) => {
             state.currentView = "default";
+            state.activeMenu = "Current Orders";
+        },
+        getCurrentView: (state) => {
+            return state.currentView;
         },
     },
 });
 
-export const { setDeliveryPartnerView, clearDeliveryPartnerView } = deliveryPartnerViewSlice.actions;
+export const { setDeliveryPartnerView, clearDeliveryPartnerView, getCurrentView } = deliveryPartnerViewSlice.actions;
 export default deliveryPartnerViewSlice.reducer;
